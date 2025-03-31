ed920bcab2d77bd4b289bdf2119575fd771fe351
sandipannas <sandisingha@gmail.com>
2025-04-01 01:47:51 +0530

    the medium palindrome question is done

---
+++ 01-js/medium/palindrome.js
@@ -0,0 +1,22 @@
+/*
+  Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
+  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
+*/
+
+function isPalindrome(str) {
+  str=str.toLowerCase()
+  str=[...str].filter(char=>char>='a' && char<='z');
+  let left=0;
+  let right=str.length-1;
+
+  while(left<right){
+    
+    if(str[left]!=str[right]){
+      return false;
+    }
+    left++; right--;
+  }
+  return true;
+}
+
+module.exports = isPalindrome;
